4c9d0a495654210ec416046a1b3d363a9bf5f369
Grigory <v240370@gmail.com>
2021-06-22 01:31:46 +0300

    Order with images without address

---
+++ lib/config/api.dart
@@ -0,0 +1,16 @@
+final String _path = "https://admin.hunter-delivery.ru/";
+// final String _path = "https://www.abg-studio.com/markets/";
+//final String _path = "https://www.abg-studio.com/market_admin/";
+// final String _path = "https://www.abg-studio.com/vmarkets_admin/";
+// final String _path = "http://10.0.2.2/vmarkets_admin/";
+// final String _path = "http://10.0.2.2/market_admin/";
+// final String _path = "http://10.0.2.2/restaurants/";
+// final String _path = "https://okmaa.in/adminPanel/";sca
+// final String _path = "http://www.pickygrab.com/admin/";
+// final String _path = "https://nktt.11nines.cloud/";
+// final String _path = "https://madir.com.ng/AdminPanel/";
+// final String _path = "https://providers.hello128.com/";
+
+String serverPath = "${_path}public/api/";
+String serverImages = "${_path}public/images/";
+String serverImgNoUserPath = "${_path}public/img/user.png";
